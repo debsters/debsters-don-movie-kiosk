929bd8ed29626e707ec28f25deed8bdea0830dfd
Deborah Barcenas <deborah.barcenas@gmail.com>
2022-09-07 15:42:33 -0400

    before polyglot.js

---
+++ src/containers/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from '../routes/home/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/routes/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '../../components/Button';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Button />
+    </div>
+  );
+};
+
+export default Home;
